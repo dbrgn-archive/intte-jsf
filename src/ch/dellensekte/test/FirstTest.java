2b7fb1d553eece0e5937bfe823645b44481685e7
cfaessler <cfaessle@hsr.ch>
2013-10-30 16:12:37 +0100

    initial commit

---
+++ src/ch/dellensekte/test/FirstTest.java
@@ -0,0 +1,14 @@
+package ch.dellensekte.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class FirstTest {
+
+	@Test
+	public void fristtest() {
+		assertTrue(true);
+
+	}
+}
